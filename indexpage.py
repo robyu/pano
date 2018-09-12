a098cc118de8acd62571b03a5c9759b8b83a7bb5
ryu <robert.yu@gmail.com>
2018-09-11 22:14:58 -0700

    new indexpage

--- indexpage.py
+++ indexpage.py
@@ -8,157 +8,178 @@ import dtutils
   
 """
 class IndexPage:
-    #
-    # cam_summary_rows
-    # cam_status_rows
+# <!-- 
+#      placeholders:
+#      {camera_overview_rows}
+#      {camera_status_cards}
+# -->
     templ_webpage = unicode("""
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta charset="utf-8">
+        <META HTTP-EQUIV="refresh" CONTENT="300"> 
 
-    <title>Panopticon</title>
+        <title>Panopticon</title>
 
-    <meta name="description" content="panopticon">
-    <meta name="author" content="Robert Yu, Buttersquid Inc">
+        <meta name="description" content="panopticon">
+        <meta name="author" content="Robert Yu, Buttersquid Inc">
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
+	<link rel="stylesheet" href="css/styles.css">
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body >
 
-  </head>
-  <body>
+        <div class="container">  <!- container: allow margins -->
+	    <div class="row">
+		<h2>
+		    Panopticon
+		</h2>
+            </div>
 
-    <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-		    <h3>
-			Panopticon
-		    </h3>
-                    <!--  SUMMARY TABLE HEADER -->
-		    <table class="table table-sm">
-			<thead>
-			    <tr>
-				<th>
-				    Camera Name
-				</th>
-				<th>
-				    Description
-				</th>
- 				<th>
-				    Admin Login
-				</th>
- 				<th>
-				    Live Video
-				</th>
-			    </tr>
-			</thead>
-                        <!- summary rows go here ->
-                        {cam_summary_rows}
-		    </table>
-                    <!-- CAMERA STATUS HEADER -->
-		    <table class="table table-sm">
-			<thead>
-			    <tr>
-				<th>
-				    #
-				</th>
-				<th>
-				    Camera Name
-				</th>
-                                <th>
-                                    Start 
-                                </th>
-                                <th>
-                                    Stop
-                                </th>
-				<th>
-				    Status Page
-				</th>
-			    </tr>
-			</thead>
-                        <!- camera status rows go here ->
-                        {cam_status_rows}
-		    </table>
-                    
-		</div>
+            <!--  SUMMARY TABLE HEADER -->
+	    <table class="table table-sm table-striped table-dark"> <!-- compact table, striped -->
+		<thead>
+		    <tr>
+			<th>
+			    Camera Name
+			</th>
+			<th>
+			    Description
+			</th>
+ 			<th>
+			    Admin Login
+			</th>
+ 			<th>
+			    Live Video
+			</th>
+		    </tr>
+		</thead>
+                
+                <!-- SUMMARY ROWS -->
+                <tbody>
+                    {camera_overview_rows}
+                </tbody>
+	    </table>
+
+            {camera_status_cards}
+            
 	</div>
-	<div class="row">
-		<div class="col-md-12">
-			<h2>
-				Activity Summary
-			</h2>
-			<p>
-			    Here's where we put a summary plot of activity vs time
-			</p>
-			<p>
-			    <a class="btn" href="#">View details </a>
-			</p>
-		</div>
-	</div>
-    </div>
+        
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+    """)
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/scripts.js"></script>
-  </body>
+                    # <!-- TEMPLATE START camera-overview
+                         
+                    #      placeholders:
+                    #      {camera_name}
+                    #      {camera_descr}
+                    #      {admin_url}
+                    #      {live_url}
+                    # -->
+    templ_overview_row = unicode("""
+                    <tr class="table-active" >
+                        <td>
+                            {camera_name}
+                        </td>
+                        <td>
+                            {camera_descr}
+                        </td>
+                        <td>
+                            <a href="{admin_url}">admin_url</a>
+                        </td>
+                        <td>
+                            <a href="live_url">live-link</a>
+                        </td>
+                    </tr>
+                    <!--  TEMPLATE END -->
+    """)
 
+            # <!-- TEMPLATE START camera-table-card
+
+            #      placeholders:
+            #      {camera_name}
+            #      {all_table_rows}
+            # -->
+    templ_camera_card = unicode("""
+            <div class="card">
+                <div class="card-title m-0" id="heading-{camera_name}">
+                    <button class="btn btn-link p-0 m-0" data-toggle="collapse" data-target="#collapse-{camera_name}" aria-expanded="true" aria-controls="collapse-{camera_name}">
+                        <h3>
+                            {camera_name}
+                        </h3>
+                    </button>
+                </div>  <!-- END of collapsible group header -->
+
+                <!-- START of collapsible data -->
+                <div id="collapse-{camera_name}" class="collapse show" aria-labelledby="heading-{camera_name}" data-parent="#accordion">
+                    <div class="card-body p-0">
+	                <table class="table table-sm table-striped table-dark">
+		            <thead>
+		                <tr>
+			            <th>
+			                #
+			            </th>
+                                    <th>
+                                        Start 
+                                    </th>
+                                    <th>
+                                        Stop
+                                    </th>
+			            <th>
+			                Status Page
+			            </th>
+		                </tr>
+		            </thead>
+                            
+                            
+                            <!-- CAMERA STATUS ROWS -->
+                            <tbody>
+                                <!-- INSERT camera-table-rows here -->
+                                {all_table_rows}
+                            </tbody>
+                        </table>
+                    </div>
+                </div> <!-- END of collapsible data  -->
+            </div> <!-- END card -->
+            <!-- TEMPLATE END camera-table-card -->
     """)
-    #
-    # templ_row placeholders:
-    # {camera_name}
-    # {camera_descr}
-    # {admin_url}
-    # {live_url}
-    templ_summary_row = unicode("""
-                        <!-- SUMMARY ROW -->                        
-                        <tbody>
-                            <tr class="table-active" >
-                                <td>
-                                    {camera_name}
-                                </td>
-                                <td>
-                                    {camera_descr}
-                                </td>
-                                <td>
-                                    <a href="{admin_url}">admin_url</a>
-                                </td>
-                                <td>
-                                    <a href={live_url}>live-link</a>
-                                </td>
-                            </tr>
-                        </tbody>
+
+                                # <!-- TEMPLATE START camera-table-row
+                                     
+                                #      placeholders:
+                                #      {index}
+                                #      {lower_datetime} - start date
+                                #      {upper_datetime} - stop date
+                                #      {webpage_url} - URL to media page
+                                # -->
+    templ_camera_table_row = unicode("""
+                                <tr class="table-active" >
+                                    <td>
+                                        {index}
+                                    </td>
+                                    <td>
+                                        {lower_datetime}
+                                    </td>
+                                    <td>
+                                        {upper_datetime}
+                                    </td>
+                                    <td>
+                                        <a href="{webpage_url}">{webpage_url}</a>
+                                    </td>
+                                </tr> <!-- END of row -->
+                                <!-- TEMPLATE STOP camera-table-row -->
     """)
-    #
-    # templ_row placeholders:
-    # {index}
-    # {camera_name}
-    # {lower_datetime} = lower date and time
-    # {upper_datetime} = upper  date and time
-    # {webpage_url} = status webpage URL
-    templ_status_row = unicode("""
-                        <!-- CAMERA STATUS ROW -->                        
-                        <tbody>
-                            <tr class="table-active" >
-                                <td>
-                                    {index}
-                                </td>
-                                <td>
-                                    {camera_name}
-                                </td>
-                                <td>
-                                    {lower_datetime}
-                                </td>
-                                <td>
-                                    {upper_datetime}
-                                </td>
-                                <td>
-                                    <a href="{webpage_url}">{webpage_url}</a>
-                                </td>
-                            </tr>
-                        </tbody>
-    """)
+    
 
     def __init__(self, www_dir, dest_fname="index.html"):
         self.www_dir = www_dir
@@ -166,13 +187,13 @@ class IndexPage:
 
         self.dest_fname = dest_fname
 
-    def generate_summary_rows(self, cam_list):
+    def generate_overview_rows(self, cam_list):
         """
         generate HTML for summary table rows
         """
         rows_html = ''
         for cam_info in cam_list:
-            rows_html += IndexPage.templ_summary_row.format(camera_name = cam_info['name'],
+            rows_html += IndexPage.templ_overview_row.format(camera_name = cam_info['name'],
                                                             camera_descr = cam_info['description'],
                                                             admin_url=cam_info['admin_url'],
                                                             live_url = cam_info['live_url'])
@@ -183,15 +204,11 @@ class IndexPage:
         """
         generate HTML for status table rows
         """
-        rows_html = ''
-        index=0
+        cards_html = ''
         for cam_info in cam_list:
+            index=0
+            rows_html = ''
             for page_dict in cam_info['status_page_list']:
-                #
-                # each dict entry in status_page_list has these keys (see campage.py::generate)
-                # lower_time_sec
-                # upper_time_sec
-                # page_fname
                 page_fname = page_dict['page_fname']
                 
                 # STOPPED HERE
@@ -200,29 +217,29 @@ class IndexPage:
                 fmt = "%a %b %d %H:%M:%S"
                 lower_dt = dtutils.sec_to_str(page_dict['lower_time_sec'],fmt)
                 upper_dt = dtutils.sec_to_str(page_dict['upper_time_sec'],fmt)
-                rows_html += IndexPage.templ_status_row.format(index=index,
-                                                               camera_name=cam_info['name'],
+                rows_html += IndexPage.templ_camera_table_row.format(index=index,
                                                                lower_datetime = lower_dt,
                                                                upper_datetime = upper_dt,
                                                                webpage_url=page_fname)
                 index += 1
             #end
+            cards_html += IndexPage.templ_camera_card.format(camera_name=cam_info['name'],
+                                                     all_table_rows = rows_html)
         #end
-        return rows_html
-        
+        return cards_html
         
     def make_index(self, cam_list):
         """
         IN:
         cam_list:  list of per-cam dictionary objects
         """
-        summary_html = self.generate_summary_rows(cam_list)
+        overview_html = self.generate_overview_rows(cam_list)
         status_html = self.generate_status_rows(cam_list)
 
         full_dest_fname = os.path.join(self.www_dir, self.dest_fname)
         f = open(full_dest_fname, "wt")
-        f.write(IndexPage.templ_webpage.format(cam_summary_rows = summary_html,
-                                               cam_status_rows = status_html))
+        f.write(IndexPage.templ_webpage.format(camera_overview_rows = overview_html,
+                                               camera_status_cards = status_html))
         f.close()
 
         return self.dest_fname
