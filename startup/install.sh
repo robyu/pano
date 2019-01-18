b1859000728e1e0ca41254fc43294f92d182423f
ryu <robert.yu@gmail.com>
2019-01-17 21:16:23 -0800

    use rc.local to perform execute-on-boot

--- startup/install.sh
+++
@@ -1,5 +0,0 @@
-PWD=`pwd`
-ln -s $PWD/pano.service /lib/systemd/system/pano.service
-chmod 644 /lib/systemd/system/pano.service
-systemctl daemon-reload
-systemctl enable pano.service
--- startup/run_pano.sh
+++ startup/run_pano.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
 #
-# run by pano.service
+# add this to /etc/rc.local
 #
-# use "systemctl status pano" to view status
-# 
+# /home/pi/pano/startup/run_pano.sh | /usr/bin/logger
+#
+LOGGER=/usr/bin/logger
+
+echo "starting run_pano.sh"
+
 cd /home/pi/pano
-./pano.py pano-ry.json &
-./watchdog.py pano-ry.json &
+if pgrep -f "pano.py" > /dev/null
+then
+    echo "pano.py already running"
+else
+    echo "starting pano.py"
+    ./pano.py pano-ry.json &
+fi
+
+if pgrep -f "watchdog.py" > /dev/null
+then
+    echo "watchdog.py already running"
+else
+    echo "starting watchdog"
+    ./watchdog.py pano-ry.json &
+fi
+
+echo "finished run_pano.sh"
