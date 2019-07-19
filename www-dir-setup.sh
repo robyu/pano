88d9044aacc29353c39a30b6cf6c4daad0704fa7
ryu <robert.yu@gmail.com>
2019-07-19 12:35:48 -0700

    fixed www-dir-setup

--- www-dir-setup.sh
+++ www-dir-setup.sh
@@ -4,9 +4,20 @@ DIR_WWW=$1
 DIR_FTP=$2
 DIR_DERIVED=$3
 
-ln -sf $DIR_WWW/FTP/ $DIR_FTP
-ln -sf $DIR_WWW/derived $DIR_DERIVED
-ln -sf $DIR_WWW/js/ ./www/js
-ln -sf $DIR_WWW/css ./www/css
+# if FTP and derived directories are located under /media/wd2tb,
+# then set up symlinks in the www directory as follows:   
+# want the www to look like this:
+# /var/www
+#         ./FTP      -> /media/wd2tb/FTP
+#         ./derived  -> /media/wd2tb/derived
+#   	  ./js       -> /users/ryu/pano/www/js
+# 	  ./css      -> /users/ryu/pano/www/css
+CWD=$(pwd)
 
+ln -svf $DIR_FTP     $DIR_WWW/FTP
+ln -svf $DIR_DERIVED $DIR_WWW/derived 
+ln -svf "$CWD"/www/js  $DIR_WWW
+ln -svf "$CWD"/www/css $DIR_WWW
+cp www/mryuck.png      $DIR_WWW
+ls -als $DIR_WWW
 
