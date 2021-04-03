289dec317a486fdccf98aedd4e2b1aa84259084b
robyu <robert.yu@gmail.com>
2021-04-02 20:50:10 -0700

    make symlinks to FTP and derived

--- lighttpd-docker/start.sh
+++ lighttpd-docker/start.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+#
+# still needed?
 chmod a+w /dev/pts/0
+
+ln -svf /var/FTP     /var/www/localhost/htdocs/FTP
+ln -svf /var/derived /var/www/localhost/htdocs/derived
+
 exec lighttpd -D -f /etc/lighttpd/lighttpd.conf
