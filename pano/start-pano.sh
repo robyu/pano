54e99342d6f16fbb2487ca0fb169837b30da775b
robyu <robert.yu@gmail.com>
2021-04-04 12:58:10 -0700

    changed start-pano.sh to use pano-docker.json config

--- pano/start-pano.sh
+++ pano/start-pano.sh
@@ -2,9 +2,9 @@
 
 #
 # copy local www files into /var/www
-cp -r -f /home/pano/www/css        /var/www/.
-cp -r -f /home/pano/www/fonts      /var/www/.
-cp -r -f /home/pano/www/js         /var/www/.
-cp -r -f /home/pano/www/mryuck.png /var/www/.
+# cp -r -f /home/pano/www/css        /var/www/.
+# cp -r -f /home/pano/www/fonts      /var/www/.
+# cp -r -f /home/pano/www/js         /var/www/.
+# cp -r -f /home/pano/www/mryuck.png /var/www/.
 
-python3 pano.py --loglevel info --logfname pano.log  pano-chuyu-server.json
+python3 pano.py --loglevel info --logfname pano.log  pano-docker.json
